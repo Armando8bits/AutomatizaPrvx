2b4d3af5c9fdb4b150b5ac34299fbcb2df0f089d
Roque A Ramírez <109245164+Armando8bits@users.noreply.github.com>
2023-10-22 00:44:51 -0500

    idenficación de elementos de registro de Contrato completa

--- src/Acciones/eventos.py
+++ src/Acciones/eventos.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By #necesario para el de arriba
 from selenium.webdriver.common.action_chains import ActionChains #para manejar dobleClik
 from selenium.webdriver.common.keys import Keys  #para enviar teclas como ENTER, ESC, etc
 
+import time
+
 class Accion:
     def DobleClickByXpath(self, driver, strXpath):
         element = WebDriverWait(driver, 300).until(EC.element_to_be_clickable((By.XPATH,strXpath)))
@@ -21,11 +23,15 @@ class Accion:
         #funcion que obtiene las coordenadas de un elemento a y de un elemento B para hacer scroll hasta el segundo elemento.
         print("función en desarrollo")
 
-    def SelectItemDropdownById(self, driver, Id, NMes):
-        '''este metodo seleciona un mes en el dropdown mes de contrato
-        donde NMes (numero de mes) es un numero del 1 al 12'''
+    def SelectItemDropdownById(self, driver, Id, NItem):
+        '''este metodo seleciona un elemento del dropdown, donde primer elemento es 1'''
         WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.ID,Id))).click()
-        WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.XPATH,'//p-dropdownitem['+str(NMes)+']/li'))).click()
+        WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.XPATH,'//p-dropdownitem['+str(NItem)+']/li'))).click()
+
+    def SelectItemDropdownByXpath(self, driver, strXpath, NItem):
+        '''este metodo seleciona un elemento del dropdown, donde primer elemento es 1'''
+        WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.XPATH,strXpath))).click()
+        WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.XPATH,'//p-dropdownitem['+str(NItem)+']/li'))).click()
     
     def SetDateByXpath(self, driver, strXpath, date):
         '''Este método inserta fecha en un control inputDate donde date es la fecha en Formato DD/MM/AAAA'''
@@ -34,5 +40,53 @@ class Accion:
 
     def setValueCheckboxByXpath(self, driver, strXpath, value):
         '''Marca o desmarca un checkbox, donde value es 1 o 0'''
-        marcado=driver.find_element_by_xpath(strXpath).get_attribute('checked')
-        #if ():
+        #marcado=driver.find_element_by_xpath(strXpath).get_attribute('checked')
+        if (value>0):
+            driver.find_element(By.XPATH,strXpath).click() #marca o desmarca checkbox
+
+    def ControlModalByXpath(self,driver,XpathBoton,XpathSpninner1,XpathSpninner2,XpathFiltro,value,XpathItem):
+        '''Controla las modales, se le pasa por parametro el xpath del botón que lo desencadena, spinners de espera, control del filto, valor a buscar y elemento a seleccionar'''
+        #Busca botón lupa que despliega modal
+        self.DobleClickByXpath(driver, XpathBoton)
+        #espera que desaparezca el spiner de carga
+        start_time = time.time()
+        WebDriverWait(driver,90).until(EC.invisibility_of_element((By.XPATH,XpathSpninner1)))
+        end_time = time.time()
+        tiempo_total= end_time- start_time
+        print("MODAL ABIERTA:\n*** Desapareció el spinner #1 en: {:.2f} seg.".format(tiempo_total))
+
+            #salir de modal, click en la X
+            #WebDriverWait(driver,30,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/p-dynamicdialog/div/div/div[1]/div'))).click()
+        #ingresa valor a buscar,:
+        WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.XPATH, XpathFiltro))).send_keys(str(value))
+        WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.XPATH, XpathFiltro))).send_keys(Keys.RETURN)
+
+        #espera que desaparezca el segundo spiner de carga
+        start_time = time.time()
+        WebDriverWait(driver,90).until(EC.invisibility_of_element((By.XPATH,XpathSpninner2)))
+        end_time = time.time()
+        tiempo_total= end_time- start_time
+        print("*** Desapareció el spinner #2 en: {:.2f} seg. **MODAL CERRADA**\n".format(tiempo_total))
+
+        #verifica que exista el contenido en el resultado
+        #Queda para despues, por ahora solo hace click en el primer elemento que encuentra, si es que hay...
+        time.sleep(0.5) #da tiempo a que se cierre la modal
+        #Seleccionar primer elemento de la tabla de resultados
+        self.DobleClickByXpath(driver, XpathItem)
+        time.sleep(1.5)
+
+    def ControlModalSFByXpath(self,driver,XpathBoton,XpathSpninner1,XpathItem):
+        '''Controla las modales, se le pasa por parametro el xpath del botón que lo desencadena, spinner de espera y elemento a seleccionar'''
+        #Busca botón lupa que despliega modal
+        self.DobleClickByXpath(driver, XpathBoton)
+        #espera que desaparezca el spiner de carga
+        start_time = time.time()
+        WebDriverWait(driver,90).until(EC.invisibility_of_element((By.XPATH,XpathSpninner1)))
+        end_time = time.time()
+        tiempo_total= end_time- start_time
+        print("MODAL ABIERTA:\n*** Desapareció el spinner en: {:.2f} seg. **MODAL CERRADA**\n".format(tiempo_total))
+        #Queda para despues, por ahora solo hace click en el primer elemento que encuentra, si es que hay...
+        time.sleep(0.5)
+        #Seleccionar primer elemento de la tabla de resultados
+        self.DobleClickByXpath(driver, XpathItem)
+        time.sleep(1.5)
--- src/Modulos/Contratos/contratoReg.py
+++ src/Modulos/Contratos/contratoReg.py
@@ -6,8 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait #para esperar el element
 from selenium.webdriver.support import expected_conditions as EC #necesario para el de arriba
 from selenium.webdriver.common.by import By #necesario para el de arriba
 
-from selenium.webdriver.common.keys import Keys  #para enviar teclas como ENTER, ESC, etc
-
 class Contrato:
     def Registrar(self, driver):
         #para abrir el panel principal...
@@ -17,65 +15,104 @@ class Contrato:
         #Busca el menú contrato
         WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.XPATH,'/html/body/app-root/app-content-layout/div/app-side-menu/div/ul/app-menu-item[1]/a/i[2]'))).click()
         #Busca el menú Empresarial
-        WebDriverWait(driver,90,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/app-root/app-content-layout/div/app-side-menu/div/ul/app-menu-item[1]/div/div/app-menu-item[1]/a'))).click()
+        WebDriverWait(driver,90,2).until(EC.element_to_be_clickable((By.XPATH,'/html/body/app-root/app-content-layout/div/app-side-menu/div/ul/app-menu-item[1]/div/div/app-menu-item[1]/a'))).click()
             #Busca el menú Activaión
             #WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/app-root/app-content-layout/div/app-side-menu/div/ul/app-menu-item[1]/div/div/app-menu-item[1]/div/div/app-menu-item[1]'))).click()
         #Busca el menú Gestión
-        WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.XPATH,'/html/body/app-root/app-content-layout/div/app-side-menu/div/ul/app-menu-item[1]/div/div/app-menu-item[1]/div/div/app-menu-item[2]'))).click()
+        WebDriverWait(driver,90,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/app-root/app-content-layout/div/app-side-menu/div/ul/app-menu-item[1]/div/div/app-menu-item[1]/div/div/app-menu-item[2]'))).click()
         #Busca botón "CREAR"
         WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.XPATH,'/html/body/app-root/app-content-layout/div/app-contrato-empresarial-index/p-panel/div/div[1]/div/div/button[2]'))).click()
-        #YA EN NUEVA PANTALLA: Busca botón lupa de "Ciudad"
-        Evento.DobleClickByXpath(driver, '/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/p-panel/div/div[2]/div/div/p-tabview/div/div[2]/p-tabpanel/div/app-contrato-empresarial-info-edit/form/p-panel[1]/div/div[2]/div/div/div/app-dropdown/div/p-dropdown/div/div[2]')
-        #buscar si se abrió la modal
-        #WebDriverWait(driver,90).until(EC.visibility_of((By.XPATH,'/html/body/p-dynamicdialog/div/div'))).click()
-        #print("***Apareció la modal...")
-        #espera que desaparezca el spiner de carga
-        start_time = time.time()
-        WebDriverWait(driver,90).until(EC.invisibility_of_element((By.XPATH,'/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/app-spinner')))
-        end_time = time.time()
-        tiempo_total= end_time- start_time
-        print("***Desapareció el spinner...en: " + str(tiempo_total))
-
-        #salir de modal, click en la X
-        #WebDriverWait(driver,30,1).until(EC.element_to_be_clickable((By.XPATH,'/html/body/p-dynamicdialog/div/div/div[1]/div'))).click()
-
-        #ingresa nombre de empresa:
-        WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/p-panel/div/div[2]/div/p-table/div/div/table/thead/tr[2]/th[3]/p-columnfilter/div/p-columnfilterformelement/input'))).send_keys(str("pichincha"))
-        WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.XPATH,'/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/p-panel/div/div[2]/div/p-table/div/div/table/thead/tr[2]/th[3]/p-columnfilter/div/p-columnfilterformelement/input'))).send_keys(Keys.RETURN)
-
-#espera que desaparezca el spiner de carga
-        start_time = time.time()
-        WebDriverWait(driver,90).until(EC.invisibility_of_element((By.XPATH,'/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/app-spinner')))
-        end_time = time.time()
-        tiempo_total= end_time- start_time
-        print("***Desapareció el 2do spinner...en: " + str(tiempo_total))
-
-        #verifica que exista el contenido en el resultado
-        #Queda para despues, por ahora solo hace click en el primer elemento que encuentra, si es que hay...
-
-        time.sleep(0.5) #da tiempo a que se cierre la modal
-
-        #Seleccionar primer elemento de la tabla de resultados
-        Evento.DobleClickByXpath(driver, '/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/p-panel/div/div[2]/div/p-table/div/div/table/tbody/tr[1]')
-        time.sleep(1.5)
+        
+        #---------YA EN NUEVA PANTALLA: Busca botón lupa de "Ciudad" para desplegar modal
+        Evento.ControlModalByXpath(driver,'/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/p-panel/div/div[2]/div/div/p-tabview/div/div[2]/p-tabpanel/div/app-contrato-empresarial-info-edit/form/p-panel[1]/div/div[2]/div/div/div/app-dropdown/div/p-dropdown/div/div[2]',
+                                   '/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/app-spinner',
+                                   '/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/app-spinner',
+                                   '/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/p-panel/div/div[2]/div/p-table/div/div/table/thead/tr[2]/th[3]/p-columnfilter/div/p-columnfilterformelement/input',
+                                   'pichincha','/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/p-panel/div/div[2]/div/p-table/div/div/table/tbody/tr[1]')
 
         #scroll abajo para que elemento sea visible
-        driver.execute_script("window.scrollTo(0,document.body.scrollHeight*0.25)") #*0.25 para que baje menos de lo normal
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight*0.35)") #*0.25 para que baje menos de lo normal
         time.sleep(1.5) #DA TIEMPO A QUE BAJE LA PAGINA
 
-        #da click en dropdown MES
         #WebDriverWait(driver,90).until(EC.element_to_be_clickable((By.XPATH,'/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/p-panel/div/div[2]/div/div/p-tabview/div/div[2]/p-tabpanel/div/app-contrato-empresarial-info-edit/form/p-panel[3]/div/div[2]/div/div[1]/div[2]/p-dropdown/div'))).click()
         Evento.SelectItemDropdownById(driver,'pr_id_10_label',2) #selecciona mes
-        Evento.SelectItemDropdownById(driver,'pr_id_9_label',1) #selecciona año
-        Evento.SetDateByXpath(driver,"(//input[@type=\'text\'])[10]","25/11/2023") #selecciona año
-
-        ''' esta es la logica si se puede hacer la comparación con el verdadero id o xpath
-        checkbox = (driver.find_element(By.XPATH,'//p-checkbox/div/div[2]').is_selected())
+        Evento.SelectItemDropdownById(driver,'pr_id_9_label',2) #selecciona año
+        Evento.SetDateByXpath(driver,"(//input[@type=\'text\'])[10]","25/02/2023") #selecciona año
+        '''
+        checkbox_original='//p-checkbox/div/div[2]'
+        checbox=''
+         #esta es la logica si se puede hacer la comparación con el verdadero id o xpath
+        checkbox = (driver.find_element(By.XPATH,checbox).is_selected())
         print((checkbox))
-        driver.find_element(By.XPATH,'//p-checkbox/div/div[2]').click() #marca o desmarca checkbox
+        driver.find_element(By.XPATH,checkbox_original).click() #marca o desmarca checkbox
         time.sleep(4)
-        checkbox = (driver.find_element(By.XPATH,'//p-checkbox/div/div[2]').is_selected())
+        checkbox = (driver.find_element(By.XPATH,checbox).is_selected())
         print((checkbox))
         '''
+        #establece chec de vigencia inmediata
+        Evento.setValueCheckboxByXpath(driver,'//p-checkbox/div/div[2]',1) #1 activa, cero no lo toca
 
-        driver.find_element(By.XPATH,'//p-checkbox/div/div[2]').click() #marca o desmarca checkbox
+        #scroll abajo para que elemento sea visible
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight*0.5)") #0.5 para que baje menos de lo normal
+        time.sleep(1.5) #DA TIEMPO A QUE BAJE LA PAGINA
+
+        #el orquestador envia la variable a asignar al ejecutivo si ejecutivo interno está vacio solo selecciona el jefe de venta
+        EjecutivoInterno="r"
+        if len(EjecutivoInterno)>0: #si viene contenido trabaja con esta modal
+            #controla modal de Ejecutivo Interno
+            Evento.ControlModalByXpath(driver,'//div[2]/div/div/div/div/app-dropdown/div/button/span',
+                                   '/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/app-spinner',
+                                   '/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/app-spinner',
+                                   '//th[4]/p-columnfilter/div/p-columnfilterformelement/input',
+                                   EjecutivoInterno,'//td[4]')
+        else: #si no, solo configura equipo de venta
+            #controla modal de Equipo Venta / Jefe Venta
+            Evento.ControlModalSFByXpath(driver,'//div[2]/div/app-dropdown/div/button/span',
+                                   '/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/app-spinner',
+                                   '//td[5]') #por defecto escoge el primer equipo
+        
+        time.sleep(0.5) #DA TIEMPO
+
+        #controla modal de Ejecutivo Externo
+        Evento.ControlModalByXpath(driver,'//div[2]/app-dropdown/div/button/span',
+                                   '/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/app-spinner',
+                                   '/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/app-spinner',
+                                   '//th[3]/p-columnfilter/div/p-columnfilterformelement/input',
+                                   't','//td[3]')
+
+        #controla modal de Ciudad Contrato
+        Evento.ControlModalByXpath(driver,'//p-panel[5]/div/div[2]/div/div/div/div/app-dropdown/div/button/span',
+                                   '/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/app-spinner',
+                                   '/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/app-spinner',
+                                   '//th[3]/p-columnfilter/div/p-columnfilterformelement/input',
+                                   'g','//td[3]')
+        
+        driver.find_element(By.ID, 'codigoPostal').send_keys("080506") #ingresa codigo postal
+        Evento.SelectItemDropdownByXpath(driver,'//div[2]/div/p-dropdown/div/span',2) #Documento de débito
+        Evento.setValueCheckboxByXpath(driver,'//div[2]/div/div/div[2]/div[2]/p-checkbox/div/div[2]',1) #en esta caso si está activado lo desactiva
+
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") #para que baje todo lo faltante
+        time.sleep(2) #DA TIEMPO A QUE BAJE LA PAGINA
+
+        Evento.SelectItemDropdownByXpath(driver,'//div[3]/div/p-dropdown/div/span',2) #Tipo descuento
+        driver.find_element(By.XPATH, '//p-inputnumber/span/input').clear() #borra campo Descuento, es vital esto para que funcione la siguiente linea
+        driver.find_element(By.XPATH, '//p-inputnumber/span/input').send_keys(25.34) #ingresa Descuento
+        Evento.SelectItemDropdownById(driver,'formaPago',2)         #ingresa forma de pago
+
+        #controla modal de Banco
+        Evento.ControlModalByXpath(driver,'//div[4]/div[2]/app-dropdown/div/button/span',
+                                   '/html/body/app-root/app-content-layout/div/app-contrato-empresarial-edit/app-spinner',
+                                   '/html/body/p-dynamicdialog/div/div/div[2]/app-empresa-dialog/app-spinner',
+                                   '//th[3]/p-columnfilter/div/p-columnfilterformelement/input',
+                                   'austro','//td[3]')
+        
+        driver.find_element(By.ID, 'responsablePago').send_keys("el responsable Pago") #ingresa responsable Pago
+        driver.find_element(By.ID, 'telefonoCelular').send_keys("0987654321") #ingresa celular
+        driver.find_element(By.ID, 'depResponsablePago').send_keys("dep responsable Pago") #ingresa departamento responsable Pago
+        driver.find_element(By.ID, 'direccionRetiroCheque').send_keys("Direccion del cheque") #ingresa dirección del cheque
+
+        Evento.SelectItemDropdownById(driver,'pr_id_14_label',3)         #ingresa Periodicidad
+
+        driver.find_element(By.XPATH , '//form/div[2]/button').click()  #guarda el contrato
+
+
